59e975d627931a5fa241e6bfd8446e491ef1fdcf
Praveen Pohar <prpohar@homeaway.com>
2019-11-06 22:01:30 -0600

    testing

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,21 +2,25 @@
 #
 #
 class minecraft {
-  file { '/opt/minecraft':
+  $url = 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar'
+  $install_dir = '/opt/minecraft'
+
+  file { $install_dir:
     ensure => directory,
   }
 
-  file { '/opt/minecraft/minecraft_server.jar':
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar',
+    source => $url,
+    before => Service['minecraft'],
   }
 
   package { 'java':
     ensure => 'present',
   }
 
-  file { '/opt/minecraft/eula.txt':
-    ensure => file,
+  file { "${install_dir}/eula.txt":
+    ensure  => file,
     content => 'eula=true',
   }
 
@@ -26,7 +30,8 @@ class minecraft {
   }
 
   service { 'minecraft':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [Package['java'], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service']]
   }
 }
